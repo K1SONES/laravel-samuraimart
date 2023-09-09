aa628a7b7f6047d970284c735bd1eb803644dd1a
K1SONES <maplekaiser7@gmail.com>
2023-09-09 22:44:05 +0900

    レビュー実装と誤字修正

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Htto\Controllers\ReviewController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
