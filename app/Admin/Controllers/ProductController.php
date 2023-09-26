4e288c8dd273b2afcbab95124a5dc9e445ec4fe8
K1SONES <maplekaiser7@gmail.com>
2023-09-26 19:49:14 +0900

    商品の管理画面作成 商品画像表示

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -27,12 +28,20 @@ class ProductController extends AdminController
         $grid = new Grid(new Product());
 
         $grid->column('id', __('Id'));
-        $grid->column('name', __('Name'));
+        $grid->column('name', __('Name'))->sortable();
         $grid->column('description', __('Description'));
-        $grid->column('price', __('Price'));
-        $grid->column('category_id', __('Category id'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('price', __('Price'))->sortable();
+        $grid->column('category.name', __('Category Name'));
+        $grid->column('image', __('Image'))->image();
+        $grid->column('created_at', __('Created at'))->sortable();
+        $grid->column('updated_at', __('Updated at'))->sortable();
+
+        $grid->filter(function($filter) {
+            $filter->like('name', '商品名');
+            $filter->like('escription', '商品説明');
+            $filter->between('price', '金額 ');
+            $filter->in('category_id', 'カテゴリー')->multipleSelect(Category::all()->pluck('name', 'id'));
+        });
 
         return $grid;
     }
@@ -51,7 +60,8 @@ class ProductController extends AdminController
         $show->field('name', __('Name'));
         $show->field('description', __('Description'));
         $show->field('price', __('Price'));
-        $show->field('category_id', __('Category id'));
+        $show->field('category.name', __('Category Name'));
+        $show->field('image', __('Image'))->image();
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -70,7 +80,7 @@ class ProductController extends AdminController
         $form->text('name', __('Name'));
         $form->textarea('description', __('Description'));
         $form->number('price', __('Price'));
-        $form->number('category_id', __('Category id'));
+        $form->select('category_id', __('Category Name'))->options(Category::all()->pluck('name', 'id'));
 
         return $form;
     }
